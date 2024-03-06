75645840fe71992f5fbb9ada91d3cb51a4643120
Rico Gunawan <ricogunawan12323@gmail.com>
2024-03-06 14:35:41 +0700

    Feature: Routing

--- talent-scouting/src/App.jsx
+++ talent-scouting/src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './page/LoginPage'
 
 function App() {
 
   return (
     <>
-      <div>test</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}>
+            {/* <Route path="blogs" element={<Blogs />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ talent-scouting/src/page/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoginPage() {
+    return (
+        <div className="text-[200px]">
+            Test
+        </div>
+    )
+}
+
+export default LoginPage
